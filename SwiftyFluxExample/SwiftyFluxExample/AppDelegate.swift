12866d1ce0dbd25283208e06e31a83ef158123ae
t-osawa-009 <da87435@gmail.com>
2019-11-15 19:12:19 +0900

    Add example

---
+++ SwiftyFluxExample/SwiftyFluxExample/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SwiftyFluxExample
+//
+//  Created by Takuya Osawa on 2019/11/15.
+//  Copyright © 2019 Takuya Osawa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
