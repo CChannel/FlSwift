9ecc95081d943608fdd93f82b4b27289bf0a5420
t-osawa-009 <da87435@gmail.com>
2019-11-15 13:31:11 +0900

    Initial Commit

---
+++ SwiftyFlux/SwiftyFlux.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyFlux.h
+//  SwiftyFlux
+//
+//  Created by Takuya Osawa on 2019/11/15.
+//  Copyright © 2019 Takuya Osawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyFlux.
+FOUNDATION_EXPORT double SwiftyFluxVersionNumber;
+
+//! Project version string for SwiftyFlux.
+FOUNDATION_EXPORT const unsigned char SwiftyFluxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyFlux/PublicHeader.h>
+
+
