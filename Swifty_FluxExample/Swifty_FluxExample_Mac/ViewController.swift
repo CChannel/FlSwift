4316e43dee6f547583598949e5ff9b0855903966
t-osawa-009 <da87435@gmail.com>
2019-11-29 12:40:01 +0900

    Modify xcodeproj settings

---
+++ Swifty_FluxExample/Swifty_FluxExample_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift_FluxExample_Mac
+//
+//  Created by Takuya Osawa on 2019/11/28.
+//  Copyright © 2019 Takuya Osawa. All rights reserved.
+//
+
+import Cocoa
+
+final class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
