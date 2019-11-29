e5632c68928bd1dd3558c8d1fe9da110c34cfe62
t-osawa-009 <da87435@gmail.com>
2019-11-29 15:59:52 +0900

    Update Mac example

--- Swifty_FluxExample/Swifty_FluxExample_Mac/ViewController.swift
+++ Swifty_FluxExample/Swifty_FluxExample_Mac/ViewController.swift
@@ -7,21 +7,64 @@
 //
 
 import Cocoa
+import Swifty_Flux
+import RxSwift
+import RxCocoa
 
 final class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        store.state.number
+            .asObservable()
+            .map({ String($0) })
+            .bind(to: textField.rx.text)
+            .disposed(by: disposeBag)
     }
+    
+    @IBAction private func plusButtonTapped(_ sender: Any) {
+        NumberActionCreator.shared.dispatcher.dispatch(.increase(1))
+    }
+    
+    @IBAction private func  minusButtonTapped(_ sender: Any) {
+        NumberActionCreator.shared.dispatcher.dispatch(.decrease(1))
+    }
+    
+    private let disposeBag = DisposeBag()
+    @IBOutlet weak var textField: NSTextField!
+    private lazy var store: Store<NumberState> = {
+        let state = NumberState()
+        let store = Store(state: state)
+        store.subscribe()
+        return store
+    }()
+}
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+enum NumberAction: Action {
+    case increase(Int)
+    case decrease(Int)
+}
+
+class NumberActionCreator {
+    static let shared = NumberActionCreator()
+    private init() {}
+    let dispatcher = Swifty_Flux.Dispatcher<NumberAction>.shared
+}
+
+class NumberState: State {
+    typealias ActionType = NumberAction
+    
+    func reduce(action: NumberAction) {
+        switch action {
+        case .increase(let value):
+            number.accept(number.value + value)
+        case .decrease(let value):
+            number.accept(number.value - value)
         }
     }
-
-
+    
+    var number =  BehaviorRelay<Int>(value: 0)
 }
+
 
