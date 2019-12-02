71b2708685e3fe65c87aceea0a9e08116490dc8f
t-osawa-009 <da87435@gmail.com>
2019-12-02 13:16:55 +0900

    Add Swifty_FluxExample_tvos

---
+++ Swifty_FluxExample/Swifty_FluxExample_tvos/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Swifty_FluxExample_tvos
+//
+//  Created by Takuya Osawa on 2019/12/02.
+//  Copyright © 2019 Takuya Osawa. All rights reserved.
+//
+
+import UIKit
+import Swifty_Flux
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.state.number
+            .asObservable()
+            .map({ String($0) })
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    @IBAction private func plusButtonTapped(_ sender: Any) {
+        NumberActionCreator.shared.dispatcher.dispatch(.increase(1))
+    }
+    
+    @IBAction private func  minusButtonTapped(_ sender: Any) {
+        NumberActionCreator.shared.dispatcher.dispatch(.decrease(1))
+    }
+    // MARK: - private
+    @IBOutlet private weak var label: UILabel!
+    private let disposeBag = DisposeBag()
+    private lazy var store: Store<NumberState> = {
+        let state = NumberState()
+        let store = Store(state: state)
+        store.subscribe()
+        return store
+    }()
+}
+
+enum NumberAction: Action {
+    case increase(Int)
+    case decrease(Int)
+}
+
+class NumberActionCreator {
+    static let shared = NumberActionCreator()
+    private init() {}
+    let dispatcher = Swifty_Flux.Dispatcher<NumberAction>.shared
+}
+
+class NumberState: State {
+    typealias ActionType = NumberAction
+    
+    func reduce(action: NumberAction) {
+        switch action {
+        case .increase(let value):
+            number.accept(number.value + value)
+        case .decrease(let value):
+            number.accept(number.value - value)
+        }
+    }
+    
+    var number =  BehaviorRelay<Int>(value: 0)
+}
