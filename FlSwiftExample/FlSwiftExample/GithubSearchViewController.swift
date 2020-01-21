2ead668f7934c1d7ae886de6b4e3e02dde81799f
t-osawa-009 <da87435@gmail.com>
2020-01-21 13:12:50 +0900

    Add github api for FlSwiftExample

--- FlSwiftExample/FlSwiftExample/GithubSearchViewController.swift
+++ FlSwiftExample/FlSwiftExample/GithubSearchViewController.swift
@@ -54,8 +54,6 @@ final class GithubSearchViewController: UIViewController {
             }
         }).disposed(by: disposeBag)
         
-        
-        
         tableView.keyboardDismissMode = .onDrag
     }
     
